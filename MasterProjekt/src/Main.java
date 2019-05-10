d881b24d498f2d8aebeea797534b9c3c4fb617fe
Christian Brzeski <Christian.Brzeski@studium.uni-hamburg.de>
2019-05-10 18:42:49 +0200

    Bilder DB angelegt + triviale Anwendung die gegebenes Bild schwarz färbt

--- MasterProjekt/src/Main.java
+++ MasterProjekt/src/Main.java
@@ -1,14 +1,22 @@
 
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
+import java.util.Arrays;
+
+
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class Main
 {
    public static void main( String[] args )
    {
-      System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
-      Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
-      System.out.println( "mat = " + mat.dump() );
+	   System.load("C:\\opencv\\build\\java\\x64\\opencv_java410.dll");
+
+	   Mat img = Imgcodecs.imread("resources/images/Angelina_Jolie_009.jpg", Imgcodecs.IMREAD_GRAYSCALE);
+       
+       byte[] imgData = new byte[(int) (img.total() * img.channels())];
+       Arrays.fill(imgData, (byte) 0);
+       img.put(0, 0, imgData);
+       Imgcodecs.imwrite("resources/images/Angelina_Jolie_010.jpg", img);
+       System.out.println("test");
    }
 }
